ba5c4ea05da773c39185d75a3ed51e441f1cf0f4
negitorogithub <s_tsuruta@toridori.co.jp>
2021-01-18 16:25:40 +0900

    App.tsxでテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,29 @@
 import React from 'react'
+import { RefreshSelector } from './RefreshSelector'
+import { SimpleRefreshContoller } from './SimpleRefreshContoller'
 export const App = () => {
-    return <div>aa</div>
+    return (
+        <RefreshSelector<number, TestError>
+            onValue={<div>Success</div>}
+            onError={<div>Error</div>}
+            onLoading={<div>Loading</div>}
+            refreshController={() => {
+                const rc = new SimpleRefreshContoller({
+                    refresher: async () => {
+                        await delay(5220)
+                        return 2
+                    },
+                    errorClass: TestError,
+                })
+                rc.requestRefresh()
+                return rc
+            }}
+        ></RefreshSelector>
+    )
 }
+
+function delay(ms: number) {
+    return new Promise<void>((resolve) => setTimeout(resolve, ms))
+}
+
+class TestError {}
