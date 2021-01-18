da2777de9ff9bde4abc9e132e34b3041a7e18873
negitorogithub <s_tsuruta@toridori.co.jp>
2021-01-18 13:13:49 +0900

    refreshStateの切り出し

--- src/RefreshController.ts
+++ src/RefreshController.ts
@@ -1,48 +1,5 @@
 import { Subject } from 'rxjs'
-
-export class RefreshState<V, E> {
-    readonly value: V | undefined
-    readonly error: E | undefined
-    readonly isRefreshing: boolean
-
-    get isSuccess() {
-        return this.value != undefined
-    }
-
-    get hasError() {
-        return this.error != undefined
-    }
-
-    constructor({
-        value,
-        error,
-        isRefreshing,
-    }: {
-        value?: V
-        error?: E
-        isRefreshing?: boolean
-    } = {}) {
-        this.value = value ?? undefined
-        this.error = error ?? undefined
-        this.isRefreshing = isRefreshing ?? false
-    }
-
-    copyWith({
-        value,
-        error,
-        isRefreshing,
-    }: {
-        value?: V
-        error?: E
-        isRefreshing?: boolean
-    }) {
-        return new RefreshState<V, E>({
-            value: value ?? this.value,
-            error: error ?? this.error,
-            isRefreshing: isRefreshing ?? this.isRefreshing,
-        })
-    }
-}
+import { RefreshState } from './RefreshState'
 
 export abstract class RefreshController<V, E> {
     readonly refresher: (() => Promise<V>) | undefined
